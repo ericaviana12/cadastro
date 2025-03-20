702964f68b299f375d64a89ee3251af367d0812c
ERICA VIANA SOARES <erica.vsoares@senacsp.edu.br>
2025-03-20 19:24:55 -0300

    Atualização

--- database.js
+++ database.js
@@ -25,10 +25,10 @@ const conectar = async () => {
             await mongoose.connect(url) // Conectar
             conectado = true // Setar a variável
             console.log("MongoDB Conectado")
-            return true // Verificação do main
+            return true // Verificação para o main
         } catch (error) {
             console.log(error)
-            return false // Verificação do main
+            return false // Verificação para o main
         }
     }
 }
